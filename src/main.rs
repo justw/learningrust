6400c9fc31805bc83e81bdde77f796a45be39b87
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2015-10-31 22:14:04 +0100

    Introduced mutable variables and reading from standard input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use std::io;
+
 fn main() {
 
-	println!("Learning Rust");
+	println!("Take a guess:");
+
+	let mut guess = String::new();
+
+	io::stdin()
+		.read_line(&mut guess)
+		.ok()
+		.expect("Failed to read line");
+
+	println!("You guessed: {}", guess);
 
 }
